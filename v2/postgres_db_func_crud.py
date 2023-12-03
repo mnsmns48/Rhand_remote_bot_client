a831b6f8adcc1f9a13ab3d677fd92631e9416ae5
cifrotech <cifrotechmobile@inbox.ru>
2023-12-03 11:40:16 +0300

    fix quantity

--- v2/postgres_db_func_crud.py
+++ v2/postgres_db_func_crud.py
@@ -28,7 +28,7 @@ def upload_data(table: Type[StockTable], data: List, **kwargs) -> bool:
     with Session(engine) as connect:
         connect.execute(insert(table), data)
         connect.commit()
-        print(f'Загрузка данных в {table} на {engine.url.host} {engine.url.port} завершена')
+    print(f'Загрузка данных в {table} на {engine.url.host} {engine.url.port} завершена')
     return True
 
 
@@ -40,15 +40,17 @@ def update_data(table: Type[StockTable], data: List, **kwargs) -> bool:
         temp_list_.update({line.get('product_code'): int(line.get('quantity'))})
     engine = create_engine(url=kwargs['bind'].engine, echo=False)
     with Session(engine) as connect:
-        response = connect.execute(select(table).filter(table.code.in_(temp_list_.keys()))).fetchall()
+        response = connect.execute(select(table).filter(table.code.in_(temp_list_.keys()))).scalars().all()
         for line in response:
-            current_amount = line[0].quantity - temp_list_.get(line[0].code)
+            current_amount = line.quantity - temp_list_[line.code]
             if current_amount == 0:
-                connect.execute(delete(table).where(table.code == line[0].code))
-                print(f"\nУдаляю проданную позицию из наличия: {line[0].code} {line[0].name}\n")
+                connect.execute(delete(table).where(table.code == line.code))
+                connect.commit()
+                print(f"\nУдаляю проданную позицию из наличия: {line.code} {line.name}\n")
             else:
-                connect.execute(update(table).where(table.code == line[0].code)
+                connect.execute(update(table).where(table.code == line.code)
                                 .values(quantity=current_amount))
+                connect.commit()
                 print(f"\nМеняю количество товара на сайте:\n"
-                      f"{line[0].name}\nБыло {temp_list_.get(line[0].code)} шт. -> стало {current_amount} шт.\n")
+                      f"{line.name}\nБыло {current_amount + temp_list_[line.code]} шт. -> стало {current_amount} шт.\n")
     return True
